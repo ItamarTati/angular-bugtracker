35f4c2d63093e2833e15c63be9f1fc8dc4937975
Itamar Tati <itamar.tati1994@gmail.com>
2021-04-11 17:39:30 +0100

    added register page and material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,34 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { MainComponent } from './main/main.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    MainComponent
+    RegisterComponent  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule, 
 
   ],
   providers: [],
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
 
-  ngOnInit(): void {
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
-
+  
 }
+
+
+
+
